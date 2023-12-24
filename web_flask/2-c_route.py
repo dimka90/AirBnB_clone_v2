5480662eec8e6ac92f7d9f5b23bdf05ab8ba5896
dimka90 <dimkayilrit@gmail.com>
2023-12-24 22:26:56 +0100

    added a route that takes in a vairable and concantenate with the word 'c'

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+This module implements a simple Flask web application.
+"""
+from flask import Flask
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+
+# Decorate the entry point route
+@app.route("/", strict_slashes=False)
+def entry():
+    """
+    Handle requests to the root URL ("/").
+
+    Returns:
+    - str: A greeting message, "Hello HBNB!"
+    """
+
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def holborton():
+    """
+    Handle request to the holborton URL
+
+    Returns:
+    - str: Holborton Abreviation, "HBNB"
+    """
+
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def vairble(text):
+    """
+    Handle request with variable
+
+    Returns:
+    - str: C + string passed
+    """
+
+    return 'C {}'.format(text)
+
+
+if __name__ == "__main__":
+    # Run the Flask application on host "0.0.0.0" and port 5000
+    app.run(host="0.0.0.0", port='5000')
