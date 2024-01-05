7caabb17aedfdbf0b29e7ec0194f18c539907774
dimka90 <dimkayilrit@gmail.com>
2024-01-05 10:05:34 +0100

    fix some more big bug

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-This module implements a simple Flask web application.
-"""
+"""This module implements a simple Flask web application."""
 from flask import Flask
 
 # Create a Flask application instance
