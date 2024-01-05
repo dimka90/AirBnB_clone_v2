ba2891456d46b3126b3e54a3a946d6e10d1ae009
dimka90 <dimkayilrit@gmail.com>
2024-01-05 10:33:04 +0100

    modify some files

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,46 +1,33 @@
 #!/usr/bin/python3
-"""
-This module implements a simple Flask web application.
-"""
+'''A simple Flask web application.
+'''
 from flask import Flask, render_template
-from models.state import State
+
 from models import storage
-# Create a Flask application instance
+from models.state import State
+
+
 app = Flask(__name__)
+'''The Flask application instance.'''
+app.url_map.strict_slashes = False
 
 
-# Decorate the entry point route
-@app.route("/states_list", strict_slashes=False)
-def list_state():
-    """
-    Handle requests to the root URL ("/states_list")
-    aand fetches the state stored in the dictionary or database.
-
-    Returns:
-    - html: A html that "
-    """
-    # This is a call to the get all the state object
-    # store in the self.__objects{} dictionary or database
-    # to retrive there values
-    state_object = storage.all(State).values()
-    # Store the return values in a list []
-    state_object = list(state_object)
-    # using a lambda function to arrange the state in
-    # ascending order
-    state_object.sort(key=lambda obj: obj.name)
-    # store the sorted list in a dictionary
-    dict_obj = {"states": state_object, "title": "HBNB",
-                "header_title": "States"}
-
-    return render_template("7-states_list.html", **dict_obj)
+@app.route('/states_list')
+def states_list():
+    '''The states_list page.'''
+    all_states = list(storage.all(State).values())
+    all_states.sort(key=lambda x: x.name)
+    ctxt = {
+        'states': all_states
+    }
+    return render_template('7-states_list.html', **ctxt)
 
 
 @app.teardown_appcontext
-def flask_teardown(exception):
+def flask_teardown(exc):
     '''The Flask app/request context end event listener.'''
     storage.close()
 
 
-if __name__ == "__main__":
-    # Run the Flask application on host "0.0.0.0" and port 5000
-    app.run(host="0.0.0.0", port='5000', debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
